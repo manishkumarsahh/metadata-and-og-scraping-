43061122e2fcb7da10694789052b2894745c6345
Manish kumar sah <manishsah23121999@gmail.com>
2021-10-11 03:53:35 +0530

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+
+const express = require('express');
+const path = require('path');
+const { nextTick } = require('process');
+const port = process.env.PORT ||  8000;
+
+const app = express();
+
+ 
+
 const request = require("request-promise");
 
 const cheerio = require("cheerio");
@@ -5,42 +16,92 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 const json2csv = require("json2csv").Parser;
+const { next } = require('cheerio/lib/api/traversing');
 
-const movie = "https://www.imdb.com/title/tt0242519/";
 
-(async() => {
 
-        let imdbData = []
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('assets'));
+
+//sending index.html file
+app.get('/', function(req,res){
+    try{
+        res.sendFile(__dirname + '/index.html');
+    }catch(e){
+        res.send(e);
+    }
+   
+    
+})
+
+
+//after submitting form this is rendered
+app.post('/parse', async function(req,res){
+    try{
+
+        const pageName = req.body.pageName;
+
+        let resultArr = []
         const response = await request({
-            uri: movie,         //uri object where we want to make a request
-            headers: {           
-                accept:
-                 "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "en-US,en;q=0.9"
-            },        
+            uri: pageName,         //uri object where we want to make a request
             gzip:true,
         });
-
-
+    
+    
         const $ = cheerio.load(response)
-
-        const title =  $('h1[class="TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG"]').text()
- 
-
-        const rating = $('span[class="AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV"]').text();
-
-        imdbData.push({
-            title,
-            rating
+    
+       
+        const title =  $('title').text()
+    
+     //list of images
+        let img = [];
+    
+         $("img").each((index, image)=>{
+            let val = $(image).attr('src');
+            img.push(val);
+            
         });
+    
+        const description =  $('meta[name="description"]').attr('content');
+    
+      
+    
+        
+        let allOGList = [];
+    
 
-        const j2cp = new json2csv();
-        const csv = j2cp.parse(imdbData);// to parse the imdb data
+        //to read all the og meta data
+        $("meta[property^='og']").each((index, listOfOg)=>{
+    
+            let val = $(listOfOg).attr('content');
+    
+            allOGList.push(val);
+            
+        });
+    
+    
+    
+        
+        resultArr.push({
+            title,
+            description,
+            img,
+            allOGList
+            
+        });
+    
+        
+        return res.send(resultArr);
+    }catch(err){
+        res.send(err);
+    }
 
-        fs.writeFileSync("./imdb.csv", csv, "utf-8");   //path,name, type
-
-         console.log(csv);
+});
 
 
-})();
+app.listen(port, function(err){
+    if(err){
+        console.log("error in running server");
+    }
+    console.log("server is running");
+});
